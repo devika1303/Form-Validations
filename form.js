87ea8201e919bedf421132e3e406a507a9b09d08
Gujjula Devika <devika1303@gmail.com>
2026-09-09 16:15:05 +0530

    add event to function

--- form.js
+++ form.js
@@ -1,5 +1,10 @@
 const form = document.getElementById("registraion-form");
 
 form.addEventListener("click", function(event){
-  
+    const fullname = document.getElementById("FullName").value.trim();
+    const email = document.getElementById("EmailId").value.trim();
+    const mobile = document.getElementById("Mobile").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const confirmpassword = document.getElementById("confirmpassword").value.trim();
+
 })
